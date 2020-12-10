79441ffb1265ca4868940cffd4c936c0ed41c1ec
unknown <jlevien808@gmail.com>
2020-12-10 08:11:48 -1000

    Landing Page 1.1

--- src/Pages/landing.js
+++ src/Pages/landing.js
@@ -7,42 +7,33 @@ import CSSTransitionGroup from "react-transition-group/CSSTransitionGroup";
 class LandingPage extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClickX = this.handleClick.bind(this);
 
     this.state = {
       sideBarVar: "1",
     };
   }
 
-  componentDidMount() {
-    document.addEventListener(
-      "click",
-      this.handleClickOutside.bind(this),
-      true
-    );
-  }
+  componentDidMount() {}
 
-  componentWillUnmount() {
-    document.removeEventListener(
-      "click",
-      this.handleClickOutside.bind(this),
-      true
-    );
-  }
-  handleClickOutside(event) {
-      window.location.hash = "/dashboards/home"
-  }
+  handleClick() {}
 
   render() {
     return (
       <Fragment>
         <CSSTransitionGroup>
-          <span className="landingContent">
+          <span
+            className="landingContent"
+            onClick={() =>(window.location.hash = "/dashboards/home")}
+          >
             <h1>RayMauiYoga.com</h1>
             <br /> <br />
             <img class="spin" src="/images/DialDecal.png"></img> <br /> <br />
             <h2>Home to Yoga education in blog and video format.</h2>
-            <br /> <br /> <br /> <br />{" "}
-            <h3>Site Under Construction. 12-10-2020 Click Anywhere to Proceed.</h3>
+            <br /> <br />
+            <h3>
+              Site Under Construction. 12-10-2020 Click Anywhere to Proceed.
+            </h3>
             <br /> <br /> <br /> <br />
           </span>
         </CSSTransitionGroup>{" "}
